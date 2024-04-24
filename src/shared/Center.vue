12d31a2a4dde7764efd96bc3643ab54a1ce405a1
tuq <tuq_5986@163.com>
2024-04-24 20:53:24 +0800

    feat:创建Center组件

---
+++ src/shared/Center.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="center">
+    <slot :class="{'horizontal': horClass, 'veritical': verClass}"></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+ const props =  defineProps({
+    direaction: {
+        type: String as PropType<'-' | '|' | 'horizontal' | 'vertical'>,
+        default: 'horizontal'
+    }
+  }) 
+
+  const direactionMap = {
+    '-': 'horizontal',
+    '|': 'vertical',
+    'horizontal': 'horizontal',
+    'vertical': 'vertical'
+  }
+
+  const extraClass = direactionMap[props.direaction]
+  const horClass = 'horizontal' === extraClass ? true : false
+  const verClass = 'veritical' === extraClass ? true: false
+    
+</script>
+
+<style lang="scss" scoped>
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    > .horizontal {
+        flex-direction: row;
+    }
+
+    > .veritical {
+        flex-direction: column;
+    }
+  }
+</style>
--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="Start">
+    <nav>menu</nav>
+
+    <div class="pig_wrapper">
+      <Center class="pig">
+        <svg style="width: 128px;">
+          <use xlink:href="#icon-pig" fill="red"></use>
+        </svg>
+      </Center>
+    </div>
+
     <div class="btn_wrapper">
         <Button class="btn"></Button>
     </div>
@@ -10,7 +20,7 @@
 <script setup>
 import Button from '../shared/Button.vue'
 import FloatButton from '../shared/FloatButton.vue';
-  
+import Center from '../shared/Center.vue'
 </script>
 
 <style lang="scss" scoped>
@@ -18,6 +28,12 @@ import FloatButton from '../shared/FloatButton.vue';
     width: 100%;
     &_wrapper {
         padding: 16px;
+    }
+  }
+
+  .pig {
+    &_wrapper {
+      padding: 160px 0;
     }
   }
 </style>
