bc0811ceec8f43afd79c7a7e535888e177064b21
tuq <tuq_5986@163.com>
2024-04-30 17:16:01 +0800

    perf:优化数字键盘，砍去多余需求

--- src/components/Item/InputPad.vue
+++ src/components/Item/InputPad.vue
@@ -5,10 +5,10 @@
         <svgIcon name="date" color="white" width="24" height="24"></svgIcon>
         <span @click="showPopup">{{ FormData() }}</span>
         <van-popup v-model:show="show" position="bottom" :style="{ height: '50%' }">
-          <van-date-picker v-model="nowDate" title="选择年月" @confirm="onConfirm" />
+          <van-date-picker v-model="nowDate" title="选择年月" @confirm="onConfirm()" @cancel="onCancel" />
         </van-popup>
       </span>
-      <span class="amount">199.9</span>
+      <span class="amount">{{amount}}</span>
     </div>
     <div class="button_Wrapper">
       <button v-for="(item,index) in buttonMap" :key="index" @click="item.onClick">{{item.text}}</button>
@@ -19,46 +19,46 @@
 <script lang="ts" setup>
 import { time } from '../../shared/Time';
 import { ref } from 'vue';
-// import { Popup } from 'vant';
 
 const buttonMap = [
-  {text: '1',onClick: () => {}},
-  {text: '2',onClick: () => {}},
-  {text: '3',onClick: () => {}},
-  {text: '清空',onClick: () => {}},
-  {text: '4',onClick: () => {}},
-  {text: '5',onClick: () => {}},
-  {text: '6',onClick: () => {}},
-  {text: '+',onClick: () => {}},
-  {text: '7',onClick: () => {}},
-  {text: '8',onClick: () => {}},
-  {text: '9',onClick: () => {}},
-  {text: '-',onClick: () => {}},
-  {text: '.',onClick: () => {}},
-  {text: '0',onClick: () => {}},
-  {text: '删除',onClick: () => {}},
+  {text: '1',onClick: () => {appendAmount(1)}},
+  {text: '2',onClick: () => {appendAmount(2)}},
+  {text: '3',onClick: () => {appendAmount(3)}},
+  {text: '4',onClick: () => {appendAmount(4)}},
+  {text: '5',onClick: () => {appendAmount(5)}},
+  {text: '6',onClick: () => {appendAmount(6)}},
+  {text: '7',onClick: () => {appendAmount(6)}},
+  {text: '8',onClick: () => {appendAmount(8)}},
+  {text: '9',onClick: () => {appendAmount(9)}},
+  {text: '.',onClick: () => {appendAmount('.')}},
+  {text: '0',onClick: () => {appendAmount(0)}},
+  {text: '清空',onClick: () => {amount.value = ''}},
   {text: '提交',onClick: () => {}},
 
 ]
 
 const now = new Date()
-
 const nowDate = ref(time(now).FormData())
 const FormData = () => {
   return nowDate.value.join("-")
 }
 
 
-
-
 const show = ref(false);
 const showPopup = () => {
   show.value = true;
 };
-
 const onConfirm = () => {
   show.value = false;
 };
+const onCancel = () => {
+  show.value = false;
+};
+
+const amount = ref('')
+const appendAmount = (value: string|number) => {
+  amount.value += value.toString()
+}
   
 </script>
 
@@ -90,18 +90,60 @@ const onConfirm = () => {
       }
     }
     > .button_Wrapper {
-      display: flex;
-      flex-wrap: wrap;
+      display: grid;
+      grid-template-areas: 
+      "n1 n2 n3 delete"
+      "n4 n5 n6 delete"
+      "n7 n8 n9 submit"
+      "n0 n0 nd submit";
+      grid-auto-rows: 48px;
+      grid-auto-columns: 1fr;
       border-top: 1px solid var(--button-border-color);
       > button {
         border: none;
         border-right: 1px solid var(--button-border-color);
         border-bottom: 1px solid var(--button-border-color);
-        width: 25%;
-        height: 48px;
         background: transparent;
-        &:nth-child(4n){
-          border-right: none;
+        &:nth-child(1){
+          grid-area: n1;
+        }
+        &:nth-child(2){
+          grid-area: n2;
+        }
+        &:nth-child(3){
+          grid-area: n3;
+        }
+        &:nth-child(4){
+          grid-area: n4;
+        }
+        &:nth-child(5){
+          grid-area: n5;
+        }
+        &:nth-child(6){
+          grid-area: n6;
+        }
+        &:nth-child(7){
+          grid-area: n7;
+        }
+        &:nth-child(8){
+          grid-area: n8;
+        }
+        &:nth-child(9){
+          grid-area: n9;
+        }
+        &:nth-child(10){
+          grid-area: nd;
+        }
+        &:nth-child(11){
+          grid-area: n0;
+        }
+        &:nth-child(12){
+          grid-area: delete;
+        }
+        &:nth-child(13){
+          grid-area: submit;
+          background: var(--button-submit-bg);
+          color: var(--button-text-color);
         }
       }
     }
