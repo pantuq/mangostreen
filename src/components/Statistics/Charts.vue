c8e2ff7c72642d778a5b15efd3f6d7a7c5fdaeca
tuq <tuq_5986@163.com>
2024-05-07 20:55:23 +0800

    feat: 完成第三个图表

--- src/components/Statistics/Charts.vue
+++ src/components/Statistics/Charts.vue
@@ -16,13 +16,32 @@
       </ul>
       </div>
     </div>
+
     <div class="chart" ref="refDiv"></div>
+
     <div class="chart" ref="refDiv2"></div>
+
+    <div class="demo3">
+      <div class="topItem" v-for="(item,index) in betterData3" :key="index">
+        <div class="sign">
+          {{ item.tag.sign }}
+        </div>
+        <div class="bar_wrapper">
+          <div class="bar_text">
+            <span>{{ item.tag.name }} - {{ item.percent }}</span>
+            <span>￥{{ item.amount }}</span>
+          </div>
+          <div class="bar">
+            <div class="bar_inner"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, reactive, ref } from 'vue';
 import * as echarts from 'echarts';
 
 
@@ -102,7 +121,18 @@ onMounted(() => {
   myChart.setOption(option);
 })
 
-
+const data3 = reactive([
+      { tag: { id: 1, name: '房租', sign: 'x' }, amount: 3000 },
+      { tag: { id: 2, name: '吃饭', sign: 'x' }, amount: 1000 },
+      { tag: { id: 3, name: '娱乐', sign: 'x' }, amount: 900 },
+    ])
+const betterData3 = computed(() => {
+  const total = data3.reduce((sum, item) => sum + item.amount, 0)
+  return data3.map(item => ({
+    ...item,
+    percent: Math.round(item.amount / total * 100) + '%'
+  }))
+})
 </script>
 
 <style lang="scss" scoped>
@@ -140,5 +170,69 @@ onMounted(() => {
       height: 190px;
       padding: 8px;
     }
-  }
+
+
+    .demo3 {
+      margin-bottom: 128px;
+      padding: 8px;
+    }
+    .topItem {
+      margin-top: 16px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .sign {
+        flex-grow: 0;
+        flex-shrink: 0;
+        width: 48px;
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: var(--tag-bg);
+        border-radius: 50%;
+      }
+      .bar_wrapper {
+        margin-left: 8px;
+        flex-grow: 1;
+        flex-shrink: 1;
+        .bar_text {
+          color: var(--text-muted);
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        }
+        .bar {
+          height: 8px;
+          border-radius: 4px;
+          background: var(--bar-bg);
+          position: relative;
+          overflow: hidden;
+          margin-top: 4px;
+        }
+        .bar_inner {
+          position: absolute;
+          top: 0;
+          left: 0;
+          height: 100%;
+          width: 42px;
+        }
+      }
+      &:nth-child(1) {
+        .bar_inner {
+          background: #eba953;
+        }
+      }
+      &:nth-child(2) {
+        .bar_inner {
+          background: #53a867;
+        }
+      }
+      &:nth-child(3) {
+        .bar_inner {
+          background: #91cc75;
+        }
+      }
+    }
+}
 </style>
