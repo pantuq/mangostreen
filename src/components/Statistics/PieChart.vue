b4db9eb0246df9f50dd403c7cd63099b8f83c72f
tuq <tuq_5986@163.com>
2024-05-17 21:01:56 +0800

    feat:完善饼图的展示

--- src/components/Statistics/PieChart.vue
+++ src/components/Statistics/PieChart.vue
@@ -5,39 +5,67 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { PropType, onMounted, ref, watch } from "vue";
 import * as echarts from "echarts";
+import handleAmount from "../../shared/handleAmount";
+
+const props = defineProps({
+  data: Array as PropType<{ name: string; value: number }[]>
+})
+
+var myChart: echarts.ECharts;
 
 const refDiv2 = ref<HTMLDivElement>();
 onMounted(() => {
   // 基于准备好的dom，初始化echarts实例
-  var myChart = echarts.init(refDiv2.value!);
+  myChart = echarts.init(refDiv2.value!);
   // 绘制图表
   const option = {
-    grid: [{ left: 0, top: 20, right: 0 }],
+    tooltip:{
+      trigger:'item',
+      position: (point: any[], _: any, dom: { offsetWidth: any; }, _rect: any, size: { viewSize: any[]; }) => {
+        // 通过计算调整 Tooltip 的位置
+        const containerWidth = size.viewSize[0];
+        const tooltipWidth = dom.offsetWidth;
+        const maxLeft = containerWidth - tooltipWidth;
+        let left = point[0];
+        if (left > maxLeft) {
+          left = maxLeft;
+        }
+        return [left, point[1]];
+      },
+      formatter: (x: {name:string, value:number, percent: number})=>{
+        const {name,value,percent} = x
+        return `${name}: ￥${handleAmount(value)} 占比 ${percent}%`
+      },
+    },
+    grid: [{ left: 0, top: 0, right: 0 ,bottom: 0}],
     series: [
       {
         type: "pie",
-        data: [
-          {
-            value: 335,
-            name: "直接访问",
-          },
-          {
-            value: 234,
-            name: "联盟广告",
-          },
-          {
-            value: 1548,
-            name: "搜索引擎",
-          },
-        ],
-        radius: "50%",
+        data: props.data,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        },
+        radius: "90%",
       },
     ],
   };
   myChart.setOption(option);
+  
 });
+
+watch(() => props.data, () => {
+  myChart?.setOption({
+    series:[{
+      data:props.data
+    }]
+  })
+})
 </script>
 
 <style lang="scss" scoped>
