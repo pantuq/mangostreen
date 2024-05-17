d3f30053172bc5d1c9978a30da38287a706b5245
tuq <tuq_5986@163.com>
2024-05-17 21:00:56 +0800

    feat:完善折线图的展示

--- src/components/Statistics/LineChart.vue
+++ src/components/Statistics/LineChart.vue
@@ -1,37 +1,87 @@
 <template>
   <div class="LineChart">
-    <div class="chart" ref="refDiv"></div>
+    <div class="chart" id="line"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { PropType, watch } from 'vue';
+import handleAmount from '../../shared/handleAmount';
+import { onMounted } from "vue";
 import * as echarts from "echarts";
+import { Time } from '../../shared/Time';
 
-const refDiv = ref<HTMLDivElement>();
+const props = defineProps({
+  data: {
+    type: Array as PropType<[string, number][]>,
+    require: true
+  }
+})
 
+
+let myChart: echarts.ECharts | undefined = undefined
+const defaultOption = {
+  tooltip: {
+    show: true,
+    trigger: 'axis',
+    formatter: ([item]: any) => {
+      const [x, y] = item.data
+      return `${new Time(new Date(x)).Formt('YYYY年MM月DD日')} ￥${handleAmount(y)}`
+    }
+  },
+  grid: [{ left: 16, top: 0, right: 16, bottom: 20 }],
+  xAxis: {
+    type: 'time',
+    boundaryGap: ['3%', '0%'],
+    axisLabel: {
+      formatter: (value: string) => new Time(new Date(value)).Formt('MM-DD')
+    },
+    axisTick: {
+      alignWithLabel: true
+    }
+  },
+  yAxis: {
+    show: true,
+    type: 'value',
+    splitLine: {
+      show: true,
+      lineStyle: {
+        type: 'dashed'
+      }
+    },
+    axisLabel: {
+      show: false
+    }
+  }
+}
 onMounted(() => {
   // 基于准备好的dom，初始化echarts实例
-  var myChart = echarts.init(refDiv.value!);
+  // chart = echarts.init(myChart)
+  myChart = echarts.init(document.getElementById('line')!)
+  myChart.setOption(defaultOption)
+  
   // 绘制图表
-  const option = {
-    grid: [{ left: 0, top: 20, right: 0 }],
-    xAxis: {
-      type: "category",
-      data: ["A", "B", "C"],
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [120, 200, 150],
-        type: "line",
-      },
-    ],
-  };
-  myChart.setOption(option);
-});
+  myChart?.setOption({
+    ...defaultOption,
+    series:[{
+      data:props.data,
+      type:'line'
+    }]
+  })
+})
+// 为什么之前那样写就不可以
+// 只有在重新编辑代码刷新之后的一次才可以显示？
+ 
+watch(() => props.data, () => {
+  myChart?.setOption({
+      series: [
+        {
+          data: props.data
+        }
+      ]
+    })
+    console.log(props.data);
+})
 </script>
 
 <style lang="scss" scoped>
