e53c48d2299faf774a6111b9d0a48673106bcf6f
tuq <tuq_5986@163.com>
2024-05-17 17:30:46 +0800

    feat:完善登录状态部分

--- src/shared/OverLay.vue
+++ src/shared/OverLay.vue
@@ -3,26 +3,40 @@
     <div class="mask" @click="$emit('closeMask')"></div>
     <div class="overlay">
       <section>
-        <h2 class="user-status">未登录用户</h2>
-        <p class="sign-in">点击这里登录</p>
+        <div v-if="userEmail">
+          <h2 class="user-status">{{ userEmail }}</h2>
+          <p class="sign-in" @click="signOut">退出登录</p>
+        </div>
+        <div v-else>
+          <RouterLink to="/login">
+            <h2 class="user-status">未登录用户</h2>
+            <p class="sign-in">点击这里登录</p>
+          </RouterLink>
+        </div>
       </section>
       <nav class="side-navbar">
         <ul>
           <li>
-            <svgIcon name="chart" width="32px" height="32px"></svgIcon>
-            <span>统计图表</span>
+            <RouterLink to="/statistics">
+              <svgIcon name="chart" width="32px" height="32px"></svgIcon>
+              <span>统计图表</span>
+            </RouterLink>
           </li>
           <li>
-            <svgIcon name="export" width="32px" height="32px"></svgIcon>
-            <span>导出数据</span>
+            <RouterLink to="/export">
+              <svgIcon name="export" width="32px" height="32px"></svgIcon>
+              <span>导出数据</span>
+            </RouterLink>
           </li>
           <li>
-            <svgIcon name="notice" width="32px" height="32px"></svgIcon>
-            <span>记账提醒</span>
+            <RouterLink to="/notice">
+              <svgIcon name="notice" width="32px" height="32px"></svgIcon>
+              <span>记账提醒</span>
+            </RouterLink>
           </li>
           <li>
             <svgIcon name="signOut" width="32px" height="32px"></svgIcon>
-            <span>退出登录</span>
+            <span>未完待续</span>
           </li>
         </ul>
       </nav>
@@ -30,7 +44,36 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+import yierRequest1 from "../service";
+import { showConfirmDialog } from "vant";
+import router from "../router";
+const route = useRoute();
+
+const userEmail = ref<string>("");
+
+yierRequest1
+  .get({
+    url: "api/v1/me",
+  })
+  .then((res) => {
+    userEmail.value = res.resource.email;
+  });
+
+const signOut = () => {
+  showConfirmDialog({
+    title: "警告",
+    message: "确定要退出登录吗？",
+  })
+    .then(() => {
+      router.push(`/sign_in?return_to=${route.fullPath}`);
+      localStorage.removeItem("jwt");
+    })
+    .catch(() => {});
+};
+</script>
 
 <style lang="less" scoped>
 .mask {
@@ -56,6 +99,7 @@
     height: 128px;
     padding: 32px 0 44px 16px;
     .user-status {
+      overflow: hidden;
       color: var(--overlay-user-status-text-color);
       font-weight: 350;
       font-size: 24px;
