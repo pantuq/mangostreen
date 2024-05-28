3089c039b312601a7baeb904bf21d61be0275991
tuq <tuq_5986@163.com>
2024-05-28 19:25:23 +0800

    style: 完善了comingSoon页面的样式

--- src/shared/ComingSoon.vue
+++ src/shared/ComingSoon.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="ComingSoon">
+    <NavBar>
+      <template #icon>
+        <BackIcon></BackIcon>
+      </template>
+      <template #title>
+        <span>山竹记账</span>
+      </template>
+    </NavBar>
     <div class="icon_wrapper">
-        <Center class="icon">
-            <svg style="width: 128px">
-                     
-                <use xlink:href="#icon-pig" fill="red"></use>
-                   
-            </svg>
-        </Center>
+      <Center class="icon">
+        <svg style="width: 128px">
+               
+          <use xlink:href="#icon-pig" fill="red"></use>
+             
+        </svg>
+      </Center>
+      <p class="text">敬请期待</p>
     </div>
-    <p class="text">敬请期待</p>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Center from './Center.vue';
-
-  
+import BackIcon from "./BackIcon.vue";
+import Center from "./Center.vue";
+import NavBar from "./NavBar.vue";
 </script>
 
 <style lang="scss" scoped>
-  .ComingSoon {
+.ComingSoon {
+  > .icon_wrapper {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -29,14 +38,15 @@ import Center from './Center.vue';
       width: 128px;
       height: 128px;
       &__wrapper {
-          padding: 120px 0;
+        padding: 120px 0;
       }
     }
 
     .text {
-        padding: 20px;
-        text-align: center;
-        font-size: 32px;
+      padding: 20px;
+      text-align: center;
+      font-size: 32px;
     }
   }
+}
 </style>
